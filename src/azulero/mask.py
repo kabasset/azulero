46351688e0c7f99a4f3a51d5506bda9efcd84abb
Antoine Basset <antoine.basset@cnes.fr>
2025-10-02 12:36:38 +0200

    Change inpainting to avoid chessboard artifacts

--- src/azulero/mask.py
+++ src/azulero/mask.py
@@ -59,7 +59,7 @@ def hot_pixels(i, y, j, h):
 def inpaint(data: np.ndarray, mask: np.ndarray):
     if data.ndim > 2:
         return skinpaint.inpaint_biharmonic(data, mask, channel_axis=-1)
-    return cv2.inpaint(data, mask.astype(np.uint8), 3, cv2.INPAINT_TELEA)
+    return cv2.inpaint(data, mask.astype(np.uint8), 3, cv2.INPAINT_NS)
 
 
 def _resaturate(x):
