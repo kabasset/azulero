Fix normalization

src/azulero/color.py
@@ -40,15 +40,9 @@ def abmag_to_value(mag, zp):
 
 
 def stretch_iyjh(data: np.ndarray, transform: Transform):
-    blacks = abmag_to_value(np.abs(transform.bw[0]), transform.iyjh_zero_points)
-    if transform.bw[0] < 0:
-        blacks = -blacks
     whites = abmag_to_value(transform.bw[1], transform.iyjh_zero_points)
-    blacks = blacks[:, np.newaxis, np.newaxis]
-    whites = whites[:, np.newaxis, np.newaxis]
-    scaling = transform.iyjh_scaling[:, np.newaxis, np.newaxis]
-    data = (data * scaling - blacks) / (whites - blacks)
-    return asinh(data, transform.stretch)
+    scaling = (transform.iyjh_scaling / whites)[:, np.newaxis, np.newaxis]
+    return asinh(data * scaling, transform.stretch, transform.bw[0])
 
 
 def iyjh_to_rgb(data: np.ndarray, transform: Transform):
@@ -90,9 +84,9 @@ def channelwise_div(data, factors):
     return data
 
 
-def asinh(data: np.ndarray, a: float):
+def asinh(data: np.ndarray, a: float, black: float):
     return np.clip(
-        np.arcsinh(data * a) / np.arcsinh(a),
+        (np.arcsinh(data * a) - black) / (np.arcsinh(a) - black),
         0,
         1,
         dtype=np.float32,

src/azulero/mask.py
@@ -59,8 +59,8 @@ def hot_pixels(i, y, j, h):
 def inpaint(data: np.ndarray, mask: np.ndarray):
     if data.ndim > 2:
         return skinpaint.inpaint_biharmonic(data, mask, channel_axis=-1)
-    # return cv2.inpaint(data, mask.astype(np.uint8), 3, cv2.INPAINT_NS)
-    return skinpaint.inpaint_biharmonic(data, mask)
+    return cv2.inpaint(data, mask.astype(np.uint8), 3, cv2.INPAINT_NS)
+    # return skinpaint.inpaint_biharmonic(data, mask)
 
 
 def _resaturate(x):

src/azulero/process.py
@@ -116,17 +116,17 @@ def add_parser(subparsers):
         "--stretch",
         "-a",
         type=float,
-        default=1000,
+        default=200,
         metavar="FACTOR",
         help="Stretching factor `a` in `asinh(data * a) / asinh(a)`.",
     )
     parser.add_argument(
-        "--black",
+        "--offset",
         "-b",
         type=float,
-        default=-31.0,
+        default=0.25,
         metavar="VALUE",
-        help="Black point, which may be 0 for background-subtracted inputs.",
+        help="Opposite of black point relative to the white point, typically a fraction of 1.",
     )
     parser.add_argument(
         "--white",
@@ -134,10 +134,10 @@ def add_parser(subparsers):
         type=float,
         default=22.0,
         metavar="VALUE",
-        help="White point.",
+        help="White point in AB magnitude.",
     )
     parser.add_argument(
-        "--hue", type=float, default=-30, metavar="ANGLE", help="Hue shift"
+        "--hue", type=float, default=-20, metavar="ANGLE", help="Hue shift"
     )
     parser.add_argument(
         "--saturation",
@@ -174,7 +174,7 @@ def run(args):
         hue=args.hue,
         saturation=args.saturation,
         stretch=args.stretch,
-        bw=np.array([args.black, args.white]),
+        bw=np.array([-args.offset, args.white]),  # FIXME separate
     )
 
     tile, slicing = io.parse_tile(args.tile)