WIP: Fixing rotation with verticalish viewport

src/azulero/roam.py
@@ -70,7 +70,7 @@ def run(args):
     print(f"Read input image: {input}")
     image = cv2.imread(input, cv2.IMREAD_COLOR)
     image_shape = image.shape[:2]
-    print(f"- Format: {image_shape[0]} x {image_shape[1]}")
+    print(f"- Shape: {image_shape[0]} x {image_shape[1]}")
     timer.tic_print()
 
     print(f"Read sequence of key frames: {config}")
@@ -102,25 +102,31 @@ def run(args):
     timer.tic_print()
 
 
-def crop(image, params, shape):
-    # TODO optimize without rotation
-    viewport = cv2.RotatedRect(params.center, np.array(shape) / params.z, params.a_deg)
+def crop(image: np.ndarray, params, format):
+    viewport_format = np.array(format, dtype=float) / params.z / 2
+    a = params.a_deg % 360
+    viewport = cv2.RotatedRect(params.center, viewport_format, -a)
     x0, y0, w, h = viewport.boundingRect()
     x1 = x0 + w
     y1 = y0 + h
+    print(f"\t\t\t\t\t{x0}\t{y0}\t{w}\t{h}")
     if x0 < 0:
         print(f"- WARNING: min(x) < 0 ({x0})")
         x0 = 0
     if y0 < 0:
         print(f"- WARNING: max(y) min < 0 ({y0})")
         y0 = 0
-    if x1 > image.shape[0]:
-        print(f"- WARNING: max(x) > {image.shape[0]-1} ({x1-1})")
+    if x1 > image.shape[1]:
+        print(f"- WARNING: max(x) > {image.shape[1]-1} ({x1-1})")
         x1 = image.shape[0]
-    if y1 > image.shape[1]:
-        print(f"- WARNING: max(y) > {image.shape[1]-1} ({y1-1})")
-    start = np.array([x0, y0])
+    if y1 > image.shape[0]:
+        print(f"- WARNING: max(y) > {image.shape[0]-1} ({y1-1})")
+        y1 = image.shape[1]
+    offset = np.array([x0, y0])
     patch = image[y0:y1, x0:x1]
-    rotation = cv2.getRotationMatrix2D(params.center - start, params.a_deg, params.z)
-    rotated_image = cv2.warpAffine(patch, rotation, (w, h), flags=cv2.INTER_LINEAR)
-    return cv2.getRectSubPix(rotated_image, shape, params.center - start)
+    rotation = cv2.getRotationMatrix2D(params.center - offset, a, params.z)
+    rotation_format = (w, h)
+    rotated_image = cv2.warpAffine(
+        patch, rotation, rotation_format, flags=cv2.INTER_LINEAR
+    )
+    return cv2.getRectSubPix(rotated_image, format, params.center - offset)

src/azulero/sequence.py
@@ -51,15 +51,19 @@ class KeyFrames:
 
 
 def load_frames_params(
-    sequence: list, image_shape: list, fps: float, video_shape: list
+    sequence: list, image_shape: list, fps: float, video_format: list
 ):
     res = KeyFrames([], [], [])
     frame = 0
     for step in sequence:
         frame = parse_frame(step["t"], fps, frame)
-        x = None if "x" not in step else parse_coord(step["x"], image_shape[0])
-        y = None if "y" not in step else parse_coord(step["y"], image_shape[1])
-        z = None if "z" not in step else parse_zoom(step["z"], image_shape, video_shape)
+        x = None if "x" not in step else parse_coord(step["x"], image_shape[1])
+        y = None if "y" not in step else parse_coord(step["y"], image_shape[0])
+        z = (
+            None
+            if "z" not in step
+            else parse_zoom(step["z"], image_shape, video_format)
+        )
         a = None if "a" not in step else parse_a_deg(step["a"])
         res.append(frame, np.array([x, y]), z, a)
     return res
@@ -69,7 +73,7 @@ def sin_sequence(keys_values: list):
     """
     Extrapolate parameters over a sequence of frames with sin interpolation.
     """
-    res = []
+    res = []  # FIXME use first value if first frame > 0
     for start, stop in zip(keys_values[:-1], keys_values[1:]):
         res += sin_step(start, stop)
     return res
@@ -125,7 +129,7 @@ def parse_coord(text: str, image_extent: int):
     return px
 
 
-def parse_zoom(text: str, image_shape: list, video_shape: list):
+def parse_zoom(text: str, image_shape: list, video_format: list):
     """
     Parse the zoom.
     If last char is "w" (resp. "h"), zoom is relative to the image width (resp. height).
@@ -134,9 +138,9 @@ def parse_zoom(text: str, image_shape: list, video_shape: list):
     if text == "...":
         return np.nan
     if text[-1] == "w":
-        z = video_shape[0] / image_shape[0] / float(text[:-1])
+        z = video_format[0] / image_shape[1] / float(text[:-1])
     elif text[-1] == "h":
-        z = video_shape[1] / image_shape[1] / float(text[:-1])
+        z = video_format[1] / image_shape[0] / float(text[:-1])
     elif text[-1] == "%":
         z = float(text[:-1]) / 100
     else: