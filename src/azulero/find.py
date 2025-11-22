d8d0f48112f2c95aa48f7abcda7fefc512f83dec
Antoine Basset <antoine.basset@cnes.fr>
2025-11-22 19:44:27 +0100

    Refactor find.py with geojson

--- src/azulero/find.py
+++ src/azulero/find.py
@@ -3,9 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from dataclasses import dataclass
-from astropy.io import fits
+import json
 from astroquery.simbad import Simbad
 import requests
+from shapely import geometry
 import sys
 
 
@@ -41,70 +42,39 @@ def object_tiles(name: str):
     return radec_tiles(object_radec(name))
 
 
-@dataclass
-class TileBox(object):
-    ra_center: float
-    dec_center: float
-    ra_radius: float
-    dec_radius: float
-
-    def __contains__(self, radec: tuple):
-        ra, dec = radec
-        return (
-            ra > self.ra_min()
-            and ra < self.ra_max()
-            and dec > self.dec_min()
-            and dec < self.dec_max()
-        )
-        # FIXME bounds
-
-    def ra_min(self):
-        return self.ra_center - self.ra_radius
-
-    def ra_max(self):
-        return self.ra_center + self.ra_radius
-
-    def dec_min(self):
-        return self.dec_center - self.dec_radius
-
-    def dec_max(self):
-        return self.dec_center + self.dec_radius
-
-    def __repr__(self):
-        return f"[{self.ra_min()}:{self.ra_max()}, {self.dec_min()}:{self.dec_max()}]"
-
-
 class Tiling(object):
 
     def __init__(self, filename):
-        data = self._first_bintable(filename)
-        indices = data["tileId"]
-        ra_centers = data["RA"]
-        dec_centers = data["Dec"]
-        widths = data["width"]
-        heights = data["height"]
-        self.tiles = {
-            t[0]: TileBox(t[1], t[2], t[3] / 120, t[4] / 120)
-            for t in zip(indices, ra_centers, dec_centers, widths, heights)
-        }
-
-    def _first_bintable(self, filename):
-        with fits.open(filename) as f:
-            for hdu in f:
-                if isinstance(hdu, fits.BinTableHDU):
-                    return hdu.data
+        print(f"Load tiling: {filename}")
+        with open(filename) as f:
+            self.tiles = json.load(f)["features"]
+        print(f"- {len(self.tiles)} tiles loaded.")
 
     def __call__(self, radec: tuple):
-        return {t: self.tiles[t] for t in self.tiles if radec in self.tiles[t]}
+        res = []
+        point = geometry.Point(*radec)
+        for tile in self.tiles:
+            polygon = geometry.shape(tile["geometry"])
+            if polygon.contains(point):
+                index = tile["properties"]["TileIndex"]
+                mode = tile["properties"]["UseCase"]
+                center = polygon.centroid
+                distance = center.distance(point)
+                if distance < 1:
+                    print(f"- Tile: {index} ({mode})")
+                    print(f"- Distance to center: {distance:.2f}°")
+                    res.append(index)
+                # FIXME type
+                # FIXME distance to center
+        if len(res) == 0:
+            print("- WARNING: No tile found.")
+        return res
 
 
 if __name__ == "__main__":
-    tiling = Tiling("~/Downloads/field_all_sky_overview.fits")
+    tiling = Tiling("DpdMerTile.geojson")
     for arg in sys.argv[1:]:
+        print(f"\n{arg}")
         radec = object_radec(arg)
-        print(f"{arg}: {radec}")
+        print(f"- Coordinates: {radec[0]:.2f}, {radec[1]:.2f}")
         tiles = tiling(radec)
-        if len(tiles) == 0:
-            print("- WARNING: No tile found.")
-        for t in tiles:
-            print(f"- {t}: {tiles[t]}")
