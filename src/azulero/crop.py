1ebb7e4ca7443d3ee1b6bd313f561efc40a50b20
Antoine Basset <antoine.basset@cnes.fr>
2026-01-20 10:40:45 +0100

    Fix crop.py read_channel()

--- src/azulero/crop.py
+++ src/azulero/crop.py
@@ -58,7 +58,7 @@ def run(args):
     timer = Timer()
 
     print(f"Read VIS channel: {workdir}")
-    data = io.read_channel(workdir, args.input.format("VIS"))
+    data = io.read_channel(workdir, args.input.format(channel="VIS"))
     timer.tic_print()
 
     print(f"Prepare data.")
