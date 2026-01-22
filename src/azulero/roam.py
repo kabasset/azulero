d7feaebf559ed532dfc7c03b4c217194c3ca5062
Antoine Basset <antoine.basset@cnes.fr>
2026-01-22 11:41:31 +0100

    Rename widgets.py as overlay

--- src/azulero/roam.py
+++ src/azulero/roam.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import cv2
 import yaml
 
-from azulero import sequence, widgets
+from azulero import overlay, sequence
 from azulero.timing import Timer
 
 
@@ -93,13 +93,13 @@ def run(args):
     timer.tic_print()
 
     if args.scale is None:
-        scale = widgets.Scale(width=0, text="")
+        scale = overlay.Scale(width=0, text="")
     elif len(args.scale) == 0:
-        scale = widgets.Scale(width=600, text="1 arcmin")
+        scale = overlay.Scale(width=600, text="1 arcmin")
     elif len(args.scale) == 1:
-        scale = widgets.Scale(width=int(args.scale[0]), text="")
+        scale = overlay.Scale(width=int(args.scale[0]), text="")
     elif len(args.scale) == 2:
-        scale = widgets.Scale(width=int(args.scale[0]), text=args.scale[1])
+        scale = overlay.Scale(width=int(args.scale[0]), text=args.scale[1])
 
     print(f"Generate frames")
     writer = cv2.VideoWriter(
