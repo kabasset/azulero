036a967604375236e39a1c12963ed0a25f2f8e47
kabasset <antoine.basset@cnes.fr>
2026-03-19 17:04:06 +0100

    Fix orientation

--- src/azulero/gaiasky.py
+++ src/azulero/gaiasky.py
@@ -17,11 +17,13 @@ class CameraRunnable:
         self.output = api.output
         self.frame = 0
         self.params = [
-            [float(v) for v in (
+            [
+                float(v)
+                for v in (
                     p.center.ra.value,
                     p.center.dec.value,
                     p.hfov.value,
-                    p.orientation.value
+                    p.orientation.value,
                 )
             ]
             for p in params
@@ -29,36 +31,33 @@ class CameraRunnable:
 
     def run(self):
         print(f"{self.frame=}")
-        p = self.params[self.frame]
-        if self.frame == len(self.params):
-            return
-        print(p)
-        pointing = self.refsys.equatorial_to_cartesian(p[0], p[1], 1.0)
-        print(f"{pointing=}")
-        pointing = np.array(pointing)
-        print(f"{pointing=}")
-        pointing /= np.linalg.norm(pointing)
-        print(f"{pointing=}")
-        self.camera.set_direction(pointing.tolist(), True)
-        print("set_direction()")
-        self.camera.set_fov(p[2] * 9 / 16) # FIXME compute vfov with atan
-        print("set_fov()")
-        up = camera_up(pointing, p[3])
-        print(f"{up=}")
-        self.camera.set_up(up.tolist(), True)
-        print("set_up()")
-        self.frame += 1
-        print(f"{self.frame=}")
-
-    def toString(self):
-        return "azul"
+        if self.frame < len(self.params):
+            p = self.params[self.frame]
+            print(p)
+            pointing = self.refsys.equatorial_to_cartesian(p[0], p[1], 1.0)
+            print(f"{pointing=}")
+            pointing = np.array(pointing)
+            print(f"{pointing=}")
+            pointing /= np.linalg.norm(pointing)
+            print(f"{pointing=}")
+            self.camera.set_direction(pointing.tolist(), True)
+            print("set_direction()")
+            self.camera.set_fov(p[2] * 9 / 16)  # FIXME compute vfov with atan
+            print("set_fov()")
+            up = camera_up(pointing, p[3])
+            print(f"{up=}")
+            self.camera.set_up(up.tolist(), True)
+            print("set_up()")
+            self.frame += 1
+            print(f"{self.frame=}")
 
     class Java:
         implements = ["java.lang.Runnable"]
 
 
-def camera_up(pointing, angle):
-    up = np.array([-np.sin(angle), np.cos(angle), 0])
+def camera_up(pointing, orientation):
+    rad = np.deg2rad(orientation)
+    up = np.array([-np.sin(rad), np.cos(rad), 0])
     up_xyz = up - np.dot(up, pointing) * pointing
     return up_xyz / np.linalg.norm(up_xyz)
 
@@ -71,6 +70,7 @@ def roam_gaiasky(params, fps, video_format, output):
     )
     api = gateway.entry_point.apiv2
 
+    api.input.disable()
     api.camera.stop()
     api.camera.free_mode()
     api.time.stop_clock()
@@ -84,12 +84,12 @@ def roam_gaiasky(params, fps, video_format, output):
     api.output.frame_output(True)
     api.base.park_camera_runnable("azul", runnable)
     print(params)
-    while runnable.frame != len(params):
-        print(f"{runnable.frame}")
-        time.sleep(0.5)
+    while runnable.frame < len(params):
+        time.sleep(0.05)
     api.output.frame_output(False)
-    api.base.unpark_runnable("azul")
+    api.base.remove_runnable("azul")
     api.camera.stop()
+    api.input.enable()
     gateway.close()
     output_dir = gateway.output_dir
     print(output_dir)
