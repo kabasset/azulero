b97f4a15ab03f5194760341ebeac82334263992e
Antoine Basset <antoine.basset@cnes.fr>
2026-07-12 17:28:04 +0200

    [retrieve] Draft DSS spatial query

--- src/azulero/providers/dss.py
+++ src/azulero/providers/dss.py
@@ -2,6 +2,7 @@
 # SPDX-PackageSourceInfo: https://github.com/kabasset/azulero
 # SPDX-License-Identifier: Apache-2.0
 
+from astropy.coordinates import SkyCoord
 import gzip
 from io import BytesIO
 from pathlib import Path
@@ -10,7 +11,34 @@ import requests
 from azulero.providers.tiling import Tile
 
 
+def tile(res, target):
+    index, ra, dec, dsr, mode = res.split(",")
+    center = SkyCoord(ra, dec, unit="deg", frame="icrs")
+    distance = center.separation(target).value
+    return Tile(
+        index,
+        mode,
+        dsr,
+        distance,
+    )
+
+
 class DSS(object):
+
+    def query_tiles(self, radec: SkyCoord, dsrs: list[str]):
+        root = "https://eas-dps-rest-ops.esac.esa.int/REST"
+        dsrs_text = ",".join("'" + d + "'" for d in dsrs)
+        dsr_query = f"Header.DataSetRelease+IN({dsrs_text})"
+        dec_deg = radec.ra.value  # FIXME degrees
+        dec_query = f"Data.WCS.CRVAL2+BETWEEN({dec_deg - 0.51},{dec_deg + 0.51})"
+        select_text = "Data.TileIndex:Data.WCS.CRVAL1:Data.WCS.CRVAL2:Header.DataSetRelease:Data.ProcessingMode"
+        r = requests.get(
+            f"{root}?project=EUCLID&class_name=DpdMerBksMosaic&{dsr_query}&{dec_query}&fields={select_text}"
+        )
+        r.raise_for_status()
+        print(r.text)
+        lines = r.text.replace('"', "").split()
+        return [tile(l, radec) for l in lines]
 
     def query_datafiles(self, tile: Tile, dsr: str):
 
