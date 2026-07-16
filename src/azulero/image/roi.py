69a7b84ca4d6d179e627ed39fbc7fb105243ad78
kabasset <antoine.basset@cnes.fr>
2026-07-16 17:32:37 +0200

    [crop] WIP: Refactoring with cv2

---
+++ src/azulero/image/roi.py
@@ -0,0 +1,140 @@
+# SPDX-FileCopyrightText: Copyright (C) 2025-2026, Antoine Basset
+# SPDX-PackageSourceInfo: https://github.com/kabasset/azulero
+# SPDX-License-Identifier: Apache-2.0
+
+import cv2
+import numpy as np
+
+
+# Adapted https://github.com/DevJom/zoner (MIT license):
+# * Only one zone is drawn
+# * The zone is a rectangle
+# * Zoom center is relative to the image, not the viewport
+class RectSelector:
+
+    def __init__(self, image: np.ndarray, window_name: str = "azul crop"):
+        self.window_name = window_name
+        self.image = np.flipud(image)
+        self.image_shape = np.array(image.shape[:2])
+        self.region = {
+            "b": 0,
+            "t": self.image_shape[0] - 1,
+            "l": 0,
+            "r": self.image_shape[1] - 1,
+        }
+
+        self.zoom_factor = 1.0
+        self.zoom_center = self.image_shape / 2
+        self.update_viewport()
+
+        self.dragging = False
+        self.mouse_pos = (0, 0)
+        self.selection = ""  # e.g. "b" for bottom edge or "tl" for top-left corner
+        self.snap_radius = 12
+
+    def update_viewport(self):
+        self.viewport_shape = np.round(self.image_shape / self.zoom_factor).astype(int)
+        self.viewport_start = np.clip(
+            self.zoom_center - self.viewport_shape / 2,
+            0,
+            self.image_shape - self.viewport_shape,
+        )
+
+    def view_to_pix(self, pos: np.ndarray):
+        unbounded = np.round(self.viewport_start + pos / self.zoom_factor).astype(int)
+        return np.clip(unbounded, 0, self.image_shape)
+
+    def select(self):
+
+        cv2.namedWindow(self.window_name, cv2.WINDOW_GUI_NORMAL)
+
+        def mouse_handler(event, x, y, flags, params):
+
+            pos = self.view_to_pix(np.array([y, x]))
+
+            def distance(point):
+                return (point - self.viewport_start) * self.zoom_factor - pos
+
+            if event == cv2.EVENT_RBUTTONDOWN:
+                self.selection = ""
+                bottom, left = distance([self.region["b"], self.region["l"]])
+                top, right = distance([self.region["t"], self.region["r"]])
+                if bottom < self.snap_radius:
+                    self.selection += "b"
+                elif top < self.snap_radius:
+                    self.selection += "t"
+                if left < self.snap_radius:
+                    self.selection += "l"
+                elif right < self.snap_radius:
+                    self.selection += "r"
+                self.dragging = bool(self.selection)
+
+            elif event == cv2.EVENT_MOUSEMOVE:
+                self.mouse_pos = pos
+                if self.dragging:
+                    if "b" in self.selection:
+                        self.region["b"] = pos[0]
+                    elif "t" in self.selection:
+                        self.region["t"] = pos[0]
+                    if "l" in self.selection:
+                        self.region["l"] = pos[1]
+                    if "r" in self.selection:
+                        self.region["r"] = pos[1]
+
+            elif event == cv2.EVENT_RBUTTONUP:
+                self.dragging = False
+                print(self.slicing())
+
+        cv2.setMouseCallback(self.window_name, mouse_handler)
+
+        cv2.imshow(self.window_name, self.image)
+        while True:
+
+            self.update_viewport()
+
+            view = self.image[
+                int(self.viewport_start[0]) : int(
+                    self.viewport_start[0] + self.viewport_shape[0]
+                ),
+                int(self.viewport_start[1]) : int(
+                    self.viewport_start[1] + self.viewport_shape[1]
+                ),
+            ]
+            display = cv2.resize(view, self.viewport_shape)
+            cv2.rectangle(
+                display,
+                (self.region["l"], self.region["b"]),
+                (self.region["r"], self.region["t"]),
+                color=(0, 255, 0),
+                thickness=int(20 / self.zoom_factor),
+            )
+            k = cv2.waitKey(1)
+            if k == 13 or k == 27:  # Enter or Escape
+                break
+            try:
+                if cv2.getWindowProperty(self.window_name, cv2.WND_PROP_VISIBLE) < 0:
+                    break
+            except:
+                break
+            cv2.imshow(self.window_name, display)
+
+        cv2.destroyAllWindows()
+        return self.slicing()
+
+    def slicing(self):
+        return (
+            slice(
+                self.image_shape[0] - self.region["t"] - 1,
+                self.image_shape[0] - self.region["b"],
+            ),
+            slice(self.region["l"], self.region["r"] + 1),
+        )
+
+
+if __name__ == "__main__":  # FIXME rm
+    path = "/home/Euclid/Downloads/DR1/102159776/Tile_102159776.tiff"
+    factor = 10
+    image = cv2.imread(path)[::-factor, ::factor]
+    sel = RectSelector(image)
+    rect = sel.select()
+    print(rect)
