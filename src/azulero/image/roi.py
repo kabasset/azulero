166d48e77b62698a4cca68908bb3fde6af46d91d
Antoine Basset <antoine.basset@cnes.fr>
2026-07-16 19:59:09 +0200

    [crop] Fix slicing, clean RectSelector

--- src/azulero/image/roi.py
+++ src/azulero/image/roi.py
@@ -16,110 +16,109 @@ class RectSelector:
     def __init__(
         self, image: np.ndarray, downsampling: int = 1, window_name: str = "azul crop"
     ):
-        self.window_name = window_name
-        self.image = np.flipud(image[::downsampling, ::downsampling])
-        self.downsampling = downsampling
-        self.image_shape = np.array(self.image.shape[:2])
-        self.region = {
+        self._name = window_name
+        self._downsampling = downsampling
+        self._image = np.flipud(
+            image[:: self._downsampling, :: self._downsampling]
+        )  # OpenCV orientation
+        self._shape = np.array(self._image.shape[:2])
+        self._region = {
             "b": 0,
-            "t": self.image_shape[0] - 1,
+            "t": self._shape[0] - 1,
             "l": 0,
-            "r": self.image_shape[1] - 1,
+            "r": self._shape[1] - 1,
         }
 
-        self.dragging = False
-        self.mouse_pos = (0, 0)
-        self.selection = ""  # e.g. "b" for bottom edge or "tl" for top-left corner
-        self.snap_radius = 50
+        self._dragging = False
+        self._selected = ""  # e.g. "b" for bottom edge or "tl" for top-left corner
+        self._snap_radius = 50
 
-    def view_to_pix(self, pos: np.ndarray):
-        return np.clip(pos, 0, self.image_shape, dtype=int)
+    @property
+    def slicing(self):
+        return (
+            slice(
+                (self._shape[0] - self._region["t"] - 1) * self._downsampling,
+                (self._shape[0] - self._region["b"]) * self._downsampling,
+            ),
+            slice(
+                self._region["l"] * self._downsampling,
+                (self._region["r"] + 1) * self._downsampling,
+            ),
+        )
 
-    def select(self):
+    def __call__(self):
 
-        cv2.namedWindow(self.window_name, cv2.WINDOW_GUI_NORMAL)
+        cv2.namedWindow(self._name, cv2.WINDOW_GUI_NORMAL)
 
         def mouse_handler(event, x, y, flags, params):
 
-            pos = self.view_to_pix(np.array([y, x]))
+            pos = np.clip([y, x], 0, self._shape - 1, dtype=int)
 
             def distance(point):
                 return np.abs(point - pos)
 
             if event == cv2.EVENT_RBUTTONDOWN:
-                self.selection = ""
-                bottom, left = distance([self.region["b"], self.region["l"]])
-                top, right = distance([self.region["t"], self.region["r"]])
-                if bottom < self.snap_radius:
-                    self.selection += "b"
-                elif top < self.snap_radius:
-                    self.selection += "t"
-                if left < self.snap_radius:
-                    self.selection += "l"
-                elif right < self.snap_radius:
-                    self.selection += "r"
-                self.dragging = bool(self.selection)
+                self._selected = ""
+                bottom, left = distance([self._region["b"], self._region["l"]])
+                top, right = distance([self._region["t"], self._region["r"]])
+                if bottom < self._snap_radius:
+                    self._selected += "b"
+                elif top < self._snap_radius:
+                    self._selected += "t"
+                if left < self._snap_radius:
+                    self._selected += "l"
+                elif right < self._snap_radius:
+                    self._selected += "r"
+                self._dragging = bool(self._selected)
 
             elif event == cv2.EVENT_MOUSEMOVE:
-                self.mouse_pos = pos
-                if self.dragging:
-                    if "b" in self.selection:
-                        self.region["b"] = pos[0]
-                    elif "t" in self.selection:
-                        self.region["t"] = pos[0]
-                    if "l" in self.selection:
-                        self.region["l"] = pos[1]
-                    if "r" in self.selection:
-                        self.region["r"] = pos[1]
+                if self._dragging:
+                    if "b" in self._selected:
+                        self._region["b"] = pos[0]
+                    elif "t" in self._selected:
+                        self._region["t"] = pos[0]
+                    if "l" in self._selected:
+                        self._region["l"] = pos[1]
+                    if "r" in self._selected:
+                        self._region["r"] = pos[1]
 
             elif event == cv2.EVENT_RBUTTONUP:
-                self.dragging = False
-                print(self.slicing())
+                self._dragging = False
+                print(self.slicing)
 
-        cv2.setMouseCallback(self.window_name, mouse_handler)
+        cv2.setMouseCallback(self._name, mouse_handler)
 
-        cv2.imshow(self.window_name, self.image)
+        cv2.imshow(self._name, self._image)
 
         while True:
 
-            display = self.image.copy()
-            cv2.rectangle(
-                display,
-                (self.region["l"], self.region["b"]),
-                (self.region["r"], self.region["t"]),
-                color=(0, 255, 0),
-                thickness=4,
-            )
             k = cv2.waitKey(1)
             if k == 13 or k == 27:  # Enter or Escape
                 break
             try:
-                if cv2.getWindowProperty(self.window_name, cv2.WND_PROP_VISIBLE) < 0:
+                if cv2.getWindowProperty(self._name, cv2.WND_PROP_VISIBLE) < 0:
                     break
             except:
                 break
-            cv2.imshow(self.window_name, display)
+
+            display = self._image.copy()
+            cv2.rectangle(
+                display,
+                (self._region["l"], self._region["b"]),
+                (self._region["r"], self._region["t"]),
+                color=(0, 255, 0),
+                thickness=4,
+            )
+            cv2.imshow(self._name, display)
 
         cv2.destroyAllWindows()
-        return self.slicing()
-
-    def slicing(self):
-        return (
-            slice(
-                (self.image_shape[0] - self.region["t"]) * self.downsampling - 1,
-                (self.image_shape[0] - self.region["b"]) * self.downsampling,
-            ),
-            slice(
-                self.region["l"] * self.downsampling,
-                self.region["r"] * self.downsampling + 1,
-            ),
-        )
+        return self.slicing
 
 
 if __name__ == "__main__":  # FIXME rm
     path = "/home/Euclid/Downloads/DR1/102159776/Tile_102159776.tiff"
     factor = 10
     image = np.flipud(cv2.imread(path))
-    sel = RectSelector(image, factor)
-    rect = sel.select()
+    select = RectSelector(image, factor)
+    rect = select()  # TODO stretching
     print(rect)
