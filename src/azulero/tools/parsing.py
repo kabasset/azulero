a842ce4d0ee55aad63fea48e626c1777a8af9d8d
Antoine Basset <antoine.basset@cnes.fr>
2026-07-22 09:18:30 +0200

    Fix type hint for Kane's environments

--- src/azulero/tools/parsing.py
+++ src/azulero/tools/parsing.py
@@ -80,7 +80,7 @@ def parse_lengths_or_angles(text: str, references: tuple | None = None):
 
 def parse_target(
     text: str, default_slicing=None, otype=Path
-) -> tuple[otype, slice | Angle | tuple | None]:
+) -> tuple["otype", slice | Angle | tuple | None]:
     """
     Parse the path and slicing from a target string.
 
