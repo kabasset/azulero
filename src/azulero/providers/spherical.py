924ba243da4b5d311eeff13329fd433316099895
Antoine Basset <antoine.basset@cnes.fr>
2026-08-29 15:03:42 +0200

    DOC: Document ConvexPolygon

--- src/azulero/providers/spherical.py
+++ src/azulero/providers/spherical.py
@@ -107,6 +107,14 @@ def xyz_to_radec(xyz: np.ndarray) -> SkyCoord:
 
 
 class ConvexPolygon:
+    """
+    Convex spherical polygon.
+
+    The polygon must be convex and cover less than one hemisphere.
+
+    Args:
+        ra, dec: RA/dec of the polygon vertices ordered either clockwise or counter-clockwise.
+    """
 
     def __init__(self, ra, dec):
         self._xyz = radec_to_xyz(ra, dec)
@@ -114,18 +122,29 @@ class ConvexPolygon:
 
     @property
     def centroid(self):
+        """
+        Centroid coordinates.
+        """
         return xyz_to_radec(self._centroid)
 
     def __len__(self):
+        """
+        Vertex count.
+        """
         return self._xyz.shape[1]
 
     def __contains__(self, p):
+        """
+        Test whether a point lies inside the polygon.
+        """
         if isinstance(p, SkyCoord):
             p = radec_to_xyz(p.ra, p.dec)
         n = len(self)
         tol = np.finfo(float).eps
         for i in range(n):
+            # Great-circle plane normal
             normal = np.cross(self._xyz[:, i], self._xyz[:, (i + 1) % n])
+            # Point outward
             if np.dot(normal, self._centroid) > 0:
                 normal = -normal
             if np.dot(normal, p) > tol:
